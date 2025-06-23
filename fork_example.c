dd014ca58515ebc3c487491e17a8d827808c1d39
H2py <ahdosj@gmail.com>
2025-06-24 07:56:15 +0900

    Making new SIGNAL HANDLER

--- fork_example.c
+++ fork_example.c
@@ -1,23 +1,43 @@
 #include <stdio.h>
+#include <signal.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <stdlib.h>
 
-int main(void)
-{
-    pid_t pid = fork();
+volatile sig_atomic_t paused = 0;
 
-    if (pid < 0)
-    {
-        perror("fork failed");
-        return 1;
-    }
-    else if(pid == 0)
-    {
-        printf("Child Process pid : %d\n", getpid());
-    }
-    else
-    {
-        printf("Parent Process PID : %d, Child Process PID : %d\n", getpid(), pid);
+void handle_pause(int sig) {
+    paused = 1;
+    printf("[+] Received SIGUSR1 — Pausing...\n");
+    // fflush(stdout);  
+}
+
+void handle_resume(int sig) {
+    paused = 0;
+    printf("[+] Received SIGUSR2 — Resuming...\n");
+    // fflush(stdout);  
+}
+
+int main(void) {
+    signal(SIGUSR1, handle_pause);
+    signal(SIGUSR2, handle_resume);
+
+    int i = 0;
+    while (1) {
+        if (!paused) {
+            printf("Running... %d\n", i++);
+        } else {
+            printf("Paused...\n");
+        }
+        // fflush(stdout);  
+        sleep(1);
     }
     return 0;
 }
+
+// By default in c, stdout is line-buffured when connected to a terminal.
+// printf("Running..."); does not immediately print unless : It contains a newline \n or The buffer is flushed manually using fflush(stdout)
+// why we use the fflush(stdout)
+
+// When we use the printf, then string format is stored in the buffer that is temporary storage to store string
+// and If you don't add the new line character then, can't recognize that the string is done so, prevent error from this situation we need to use fflush(stdout)
+// means that remove all buffer with print all string format
