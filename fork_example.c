b3ba8eb97ceb2f22b7bfe9c1caab083d956fdec9
H2py <ahdosj@gmail.com>
2025-06-24 10:15:54 +0900

    comit commit commit

--- fork_example.c
+++ fork_example.c
@@ -41,3 +41,10 @@ int main(void) {
 // When we use the printf, then string format is stored in the buffer that is temporary storage to store string
 // and If you don't add the new line character then, can't recognize that the string is done so, prevent error from this situation we need to use fflush(stdout)
 // means that remove all buffer with print all string format
+
+// Volatile tells the compiler not to optimize anything that has to do with the volatile variable.
+// 변수의 값을 컴파일러가 레지스터 등에 저장해두고 캐싱하지 말고, 매번 메모리에서 실제 값을 읽으라는 의미
+
+// 시그널 핸들러에서 변수 값을 바꿀 때
+// 메인 프로그램이 조건을 기다리며 변수를 검사하는 중에, 시그널 핸들러가 변수 값을 바꿀 수도 있다
+// 이 경우에도 volatitle을 써야 컴파일러가 값을 항상 최신 상태로 읽게 된다
